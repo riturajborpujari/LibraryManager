05fe9f685a91911ca927f0e750e5f5ce35d1dd73
Rituraj Borpujari <riturajborpujari@gmail.com>
2018-10-28 09:08:46 +0530

    Added the structure for holding a book

---
+++ book.h
@@ -0,0 +1,18 @@
+//////////////////////////////////////////////////////////////////////////////////////////
+// File		: book.h								//
+// Author	: Rituraj Borpujari							//
+// Desc		: contains the structure for keeping details of a book.			// 
+// 		   serves as a model for LibraryManager					//
+//////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __BOOK__H__
+#define __BOOK__H__
+
+struct BOOK
+{
+	long int id;		// a unique id for each book
+	char name[60];
+	char author[30];
+};
+
+
