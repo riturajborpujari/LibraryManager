782e4302eb535e9626278cbda1c20382889eeabf
Rituraj Borpujari <riturajborpujari@gmail.com>
2018-10-29 23:26:10 +0530

    outdated copies were pushed to github: fixed

--- book.h
+++ book.h
@@ -10,7 +10,7 @@
 
 struct BOOK
 {
-	long int id;		// a unique id for each book
+	unsigned long int id;		// a unique id for each book
 	char name[60];
 	char author[30];
 };
--- main.cpp
+++ main.cpp
@@ -1,30 +1,53 @@
 #include <iostream>
+#include <vector>
+
+#include "recordsmanager.h"
 #include "book.h"
-#include "filehandler.h"
 
 using namespace std;
 
 int main()
 {
 	BOOK book;
+	RECORDS_MANAGER library;
+	vector<BOOK> bookList;
+	unsigned long int id = -1;
 
-	cout<< "Enter the name of the book...";
-	cin.getline(book.name, 60);
-	cout<< "Enter the author of the book...";
-	cin.getline(book.author, 30);
-	book.id = 1;
-
-	// write the book data to the file
-	FILE_HANDLER file = FILE_HANDLER("data.bin");
-	file.Write((char*)&book, sizeof(BOOK));
-
-	BOOK r;
-	// read the book back from file
-	file.Read((char*)&r, sizeof(BOOK));
-	cout<< r.name << ", " << r.author;
-
-	fflush(stdin);
-	cin.get();
+	int choice =0;
+	while(choice!= -1)
+	{
+		cout<<"---------------------------------------------------------------"<<endl;
+		cout<<"1(Add), 2(Delete), 3(view all), -1(exit)...Enter your choice...";
+		cin>>choice;
+		
+		fflush(stdin);
+		switch(choice)
+		{
+		case 1:
+			cout<< "Enter the name of the book...";
+			cin.getline(book.name, 60);
+			cout<< "Enter the author of the book...";
+			cin.getline(book.author, 30);
+		
+			library.AddBook(&book);
+			break;
+		case 2:
+			cout<<"Enter id of the book to be deleted...";
+			cin>>id;
+			cout<<library.DeleteBook(id);
+			break;
+		case 3:
+			library.GetAllBooks(&bookList);
+			
+			for(vector<BOOK>::iterator it = bookList.begin(); it !=bookList.end(); it++)
+			{
+				book = *(it);
+				cout<<"Name\t: "<<book.name<<"\tAuthor\t: "<<book.author<<" with id : "<<book.id<<endl;
+			}
+			//bookList.clear();
+			break;
+		}
+	}
 
 	return 0;
 }
